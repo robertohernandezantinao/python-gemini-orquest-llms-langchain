2c9208f950f071ab09406eb7457aed1cef088aec
Roberto Hernandez <robertohernandezantinao@gmail.com>
2026-07-15 23:08:17 -0400

    Utilizando prompt templates con LangChain y Creando una cadena con LCEL

--- lang_chain.py
+++ lang_chain.py
@@ -5,6 +5,8 @@ from my_models import GEMINI_FLASH
 from my_keys import GEMINI_API_KEY, COHERE_API_KEY
 from my_helper import encode_image
 import cohere
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 #llm con GEMINI_FLASH
 llm = ChatGoogleGenerativeAI(
@@ -15,39 +17,69 @@ llm = ChatGoogleGenerativeAI(
 
 imagen = encode_image('datos/ejemplo_grafico.jpg')
 
-pregunta = "Describe la imagen:"
+#pregunta = "Describe la imagen:"
 
-mensaje = HumanMessage(
-    content = [
-        {
-            "type": "text",
-            "text": pregunta
-        },
-        {
-            "type": "image_url",
-            "image_url": f"data:image/jpeg;base64,{imagen}"
-        }
+# mensaje = HumanMessage(
+#     content = [
+#         {
+#             "type": "text",
+#             "text": pregunta
+#         },
+#         {
+#             "type": "image_url",
+#             "image_url": f"data:image/jpeg;base64,{imagen}"
+#         }
+#     ]
+# )
+
+template_analisis = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Asume que eres analista de imágenes. Tu principal tarea consiste en: analizar una imagen para extraer las informaciones más relevantes de manera objetiva.
+            """
+        ),
+        (
+            "user",
+            [
+                {
+                    "type": "texto",
+                    "text": "Describe la imagen:"
+                },
+                {
+                    "type": "image_url",
+                    "image_url": "data:image/jpeg;base64,{imagen_informada}"
+                }
+            ]
+        )
     ]
 )
 
-respuesta = llm.invoke([mensaje])
-print(respuesta)
+cadena_analisis = template_analisis | llm | StrOutputParser()
 
-respuesta = llm.invoke("Cuáles canales colombianos de youtube me recomiendas para saber más sobre teléfonos inteligentes?")
-print(f"Gemini: ",respuesta.content)
+respuesta_analisis = cadena_analisis.invoke({"imagen_informada": imagen})
+
+print(respuesta_analisis)
+
+# respuesta = llm.invoke([mensaje])
+# print(respuesta)
+
+# respuesta = llm.invoke("Cuáles canales colombianos de youtube me recomiendas para saber más sobre teléfonos inteligentes?")
+# print(f"Gemini: ",respuesta.content)
 
 #ll con Cohere
-co = cohere.ClientV2(api_key=COHERE_API_KEY)
+# co = cohere.ClientV2(api_key=COHERE_API_KEY)
 
-response = co.chat(
-    model="command-a-03-2025",
-    messages=[
-        {
-            "role": "user",
-            "content": "¿Qué canales colombianos de YouTube recomiendas sobre teléfonos inteligentes?"
-        }
-    ]
-)
+# response = co.chat(
+#     model="command-a-03-2025",
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": "¿Qué canales colombianos de YouTube recomiendas sobre teléfonos inteligentes?"
+#         }
+#     ]
+# )
 
-print(response.message.content[0].text)
+# print(response.message.content[0].text)
 
